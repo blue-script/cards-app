369750fc0ff85cf8681380199fdd66c9c22d9461
blue-script <alexm92@inbox.ru>
2024-04-26 18:26:50 +0500

    add polymorphic button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { Button } from '@/components/ui/button/button'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <>
+      <div>Hello</div>
+
+      <Button>simple button</Button>
+
+      <Button as={'a'} href={'www.google.com'}>
+        Link
+      </Button>
+    </>
+  )
 }
